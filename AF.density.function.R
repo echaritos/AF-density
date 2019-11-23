2295cced1cc8f9739fe8e4442d708ed8039dfd17
echaritos <efstratios.charitos@gmail.com>
2019-11-23 11:46:25 +0100

    Add files via upload
    Initial commit

---
+++ AF.density.function.R
@@ -0,0 +1,66 @@
+# AF density function
+# Author Efstratios I. Charitos, MD, PhD efstratios.charitos@gmail.com
+# Date: 22-Nov-2019 v.3
+
+#data is a vector of daily AF minutes
+#timeunits = how many time units (data) are in one day : If data is in days then timeunits=24; if data is in minutes then timeunits=1440
+#limit=T  hardcodes a density of 1 in cases of >1 results (high burdens)
+
+AF.density<-function(data, minim=0.001, timeunits=1440, limit=T){
+
+	
+	#f2 function
+	f2<-function(minim=0.001, maxim=0.999, b=0.001,data){
+		
+		
+		f<-function(x, max) {
+			
+			#rolly  
+			s <- 0
+			len <- Inf
+			start <- 1
+			j <- 1
+			for (i in seq_along(x)) {
+				s <- s + x[i]
+				while (s >= max) {
+					if (i-j+1 < len) {
+						len <- i-j+1
+						start <- j
+					}
+					s <- s - x[j]
+					j <- j + 1
+				}
+			}
+			
+			list(start=start, length=len)
+			
+			len
+		}
+		
+		k<-NULL
+		for (m in seq(from=minim, to=maxim, by=b)){ k<-c(k,f(data,m))} 
+		k.per<-k/length(data)
+		return(k.per)
+		
+	}
+	
+	if (sum(data)==0) return(0) else { #CHECK
+	
+	maxim=1-minim
+	b=minim
+	
+	data.test<-data.frame(day=1:length(data), afmin=data)
+	burden<-sum(data.test$afmin)/timeunits/length(data.test$afmin)
+	data.test$daily.burd<-data.test$afmin/sum(data.test$afmin)
+	
+	raw.den<-(2*(sum(abs(seq(minim, maxim, b)-(f2(data.test$daily.burd, minim=minim, maxim=maxim, b=b)))*b))/(1-burden))
+  
+  density<-raw.den
+  if (limit==T & raw.den>1) density=1
+	#if (is.na(raw.den)==TRUE) density=0
+
+  return(density)
+}
+}
+
+
